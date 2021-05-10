c9d17f68129ee2082ce678e40073323c6dda4409
Janek Lauer <janek@lauer.email>
2021-05-10 21:25:17 +0200

    add edit

--- src/components/tribes.js
+++ src/components/tribes.js
@@ -1,13 +1,36 @@
 import React from 'react'
-import { List, DateField, Datagrid, TextField } from 'react-admin';
+import { TextInput, DateTimeInput, Create, Edit, SimpleForm, List, DateField, Datagrid, TextField } from 'react-admin';
+
+const TribeTitle = ({ record }) => {
+    return <span>Tribe {record ? `"${record.name}"` : ''}</span>;
+}
 
 export const TribeList = props => (
     <List {...props}>
         <Datagrid rowClick="edit">
             <TextField source="id" />
-            <DateField source="createdat" />
-            <DateField source="updatedat" />
+            <DateField source="CreatedAt" />
+            <DateField source="UpdatedAt" />
             <TextField source="name" />
         </Datagrid>
     </List>
+);
+
+export const TribeEdit = props => (
+    <Edit title={<TribeTitle />} {...props}>
+        <SimpleForm>
+            <TextInput source="name" />
+            <TextInput disabled source="id" />
+            <DateTimeInput disabled source="CreatedAt" />
+            <DateTimeInput disabled source="UpdatedAt" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const TribeCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <TextInput source="name" />
+        </SimpleForm>
+    </Create>
 );
