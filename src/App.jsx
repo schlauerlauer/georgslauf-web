efc1d88e6ac814169e0709eb6aabc8397b1a11fa
Janek Lauer <janek@lauer.email>
2021-06-07 20:08:08 +0200

    add auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import jsonServerProvider from 'ra-data-json-server';
 import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Route } from 'react-router-dom';
+import { Test } from './components/test';
+import { fetchUtils } from 'react-admin';
 
 import DescriptionIcon from '@material-ui/icons/Description';
 import FingerprintIcon from '@material-ui/icons/Fingerprint';
@@ -18,7 +21,7 @@ import { StationList, StationEdit, StationCreate } from './components/station';
 import { StationPointList, StationPointEdit, StationPointCreate } from './components/stationpoint';
 import { GroupPointList, GroupPointEdit, GroupPointCreate } from './components/grouppoint';
 import { TribeList, TribeEdit, TribeCreate } from './components/tribe';
-import { RoleList, RoleEdit, RoleCreate } from './components/role';
+import { RuleList, RuleCreate } from './components/rule';
 import { LoginList, LoginEdit, LoginCreate } from './components/login';
 import { GroupingList, GroupingEdit, GroupingCreate } from './components/grouping';
 import { ContentList, ContentEdit, ContentCreate } from './components/content';
@@ -26,16 +29,30 @@ import { GroupTopList } from './components/grouptop';
 import { StationTopList } from './components/stationtop';
 import { RunCreate, RunEdit, RunList } from './components/run';
 import { ContentTypeCreate, ContentTypeEdit, ContentTypeList } from './components/contenttype';
+import customRoutes from './components/customRoutes';
 
-const dataProvider = jsonServerProvider('http://localhost:8080/v1');
+import authProvider from './authProvider';
+import { ConfigCreate, ConfigEdit, ConfigList } from './components/config';
+
+const httpClient = (url, options = {}) => {
+  if (!options.headers) {
+      options.headers = new Headers({ Accept: 'application/json' });
+  }
+  const token = localStorage.getItem('token');
+  options.headers.set('Authorization', `Bearer ${token}`);
+  return fetchUtils.fetchJson(url, options);
+};
+
+const dataProvider = jsonServerProvider('http://localhost:8080/v1', httpClient);
+
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin authProvider={authProvider} customRoutes={customRoutes} dataProvider={dataProvider}>
     <Resource name="tribes" icon={FavoriteIcon} list={TribeList} edit={TribeEdit} create={TribeCreate}/>
     <Resource name="groups" icon={DirectionsRunIcon} list={GroupList} edit={GroupEdit} create={GroupCreate}/>
     <Resource name="stations" icon={DeckIcon} list={StationList} edit={StationEdit} create={StationCreate}/>
     <Resource name="stationpoints" icon={AssignmentTurnedInIcon} options={{ label: 'Station points' }} list={StationPointList} edit={StationPointEdit} create={StationPointCreate}/>
     <Resource name="grouppoints" icon={ControlPointIcon} options={{ label: 'Group points' }} list={GroupPointList} edit={GroupPointEdit} create={GroupPointCreate}/>
-    <Resource name="roles" icon={PolicyIcon} list={RoleList} edit={RoleEdit} create={RoleCreate}/>
+    <Resource name="rules" icon={PolicyIcon} list={RuleList} create={RuleCreate}/>
     <Resource name="logins" icon={FingerprintIcon} list={LoginList} edit={LoginEdit} create={LoginCreate}/>
     <Resource name="groupings" icon={BubbleChartIcon} list={GroupingList} edit={GroupingEdit} create={GroupingCreate}/>
     <Resource name="contents" icon={DescriptionIcon} options={{ label: 'Content' }} list={ContentList} edit={ContentEdit} create={ContentCreate}/>
@@ -43,7 +60,7 @@ const App = () => (
     <Resource name="stationtops" icon={PollIcon} options={{ label: 'Station Chart' }} list={StationTopList}/>
     <Resource name="runs" list={RunList} edit={RunEdit} create={RunCreate} icon={EventIcon}/>
     <Resource name="contenttypes" options={{ label: 'Content Types' }} list={ContentTypeList} edit={ContentTypeEdit} create={ContentTypeCreate}/>
-
+    <Resource name="configs" list={ConfigList} edit={ConfigEdit} create={ConfigCreate}/>
     <Resource icon={ScheduleIcon}/>
   </Admin>
 );
