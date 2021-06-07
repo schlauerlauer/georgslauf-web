ce45629ae1eedc471a00784ea7548f96b77689f5
Janek Lauer <janek@lauer.email>
2021-06-07 22:40:47 +0200

    fix rule

--- src/components/rule.jsx
+++ src/components/rule.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { TextInput, NumberField, ReferenceInput, SelectInput, NumberInput, DateTimeInput, EditButton, Create, Edit, SimpleForm, List, Datagrid, TextField, CheckboxGroupInput } from 'react-admin';
 
-// const RuleTitle = ({ record }) => {
-//     return <span>Rule {record ? `"${record.name}"` : ''}</span>;
-// }
+import { TopToolbar, SortButton, RefreshButton, CreateButton, ExportButton } from 'react-admin';
+
+const ListActions = () => (
+    <TopToolbar>
+        <RefreshButton/>
+        <CreateButton/>
+        <ExportButton/>
+    </TopToolbar>
+);
 
 export const RuleList = props => (
-    <List {...props}>
+    <List actions={<ListActions/>} {...props}>
         <Datagrid>
             <TextField label="Subject" source="v0"/>
             <TextField label="Object" source="v1"/>
@@ -15,20 +21,6 @@ export const RuleList = props => (
     </List>
 );
 
-// export const RuleEdit = props => (
-//     <Edit title={<RuleTitle />} {...props}>
-//         <SimpleForm>
-//             <ReferenceInput source="v0" label="Subject" reference="logins">
-//                 <SelectInput optionText="username"/>
-//             </ReferenceInput>
-//             <TextInput label="Object" source="v1"/>
-//             <TextInput label="Action" source="v2"/>
-//             <TextInput disabled source="ptype"/>
-//             <NumberInput disabled source="id"/>
-//         </SimpleForm>
-//     </Edit>
-// );
-
 export const RuleCreate = props => (
     <Create {...props}>
         <SimpleForm>
@@ -36,8 +28,7 @@ export const RuleCreate = props => (
                 <SelectInput optionText="username"/>
             </ReferenceInput>
             <TextInput label="Object" source="object"/>
-            <TextInput label="Action" source="action"/>
-            <CheckboxGroupInput source="test" choices={[
+            <CheckboxGroupInput source="action" choices={[
                 { id: 'GET', name: 'GET' },
                 { id: 'POST', name: 'POST' },
                 { id: 'PUT', name: 'PUT' },
