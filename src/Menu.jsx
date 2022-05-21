0338644c13ee83a9ba2fc4da9e63aec5f0167b88
Janek Lauer <janek@lauer.email>
2022-05-21 10:35:12 +0200

    add test route

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,15 +1,39 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { MenuItemLink, getResources } from 'react-admin';
 import DefaultIcon from '@material-ui/icons/ViewList';
 import HomeIcon from '@material-ui/icons/Home';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import { Divider } from '@material-ui/core';
+import authProvider from './authProvider';
 
 export const Menu = ({ onMenuClick, open }) => {
     const resources = useSelector(getResources);
+
+    const [tmpRole, setTmpRole] = useState();
+
+    authProvider.getPermissions().then(
+        ({role}) => {
+            setTmpRole(role);
+        }
+    );
     return (
         <div>
             <MenuItemLink to="/home" primaryText="Home" leftIcon={<HomeIcon/>}/>
+
+            {[
+                tmpRole === "admin"
+                ? [
+                    <MenuItemLink to="/test" primaryText="Test" leftIcon={<HomeIcon/>}/>,
+                ]
+                : null,
+                tmpRole === "public"
+                ? [
+                    <MenuItemLink to="/test" primaryText="Public" leftIcon={<HomeIcon/>}/>,
+                    <MenuItemLink to="/login" primaryText="Login" leftIcon={<ExitToAppIcon/>}/>,
+                ]
+                : null,
+            ]}
             <Divider/>
             {resources.map(resource => (
                 <MenuItemLink
--- src/customRoutes.jsx
+++ src/customRoutes.jsx
@@ -3,6 +3,7 @@ import Home from './Home';
 
 const routes = [
     <Route exact path="/home" component={Home}/>,
+    <Route exact path="/test"/>,
 ];
 
 export default routes;
